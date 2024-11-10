94229e502ccc8fccc511050883ac30c86c572f50
NaziaImran786 <naziaimran.4df@gmail.com>
2024-11-10 18:59:22 +0500

    up

--- src/app/components/MenuOverlay.tsx
+++ src/app/components/MenuOverlay.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import NavLink from './NavLink'
 
-const MenuOverlay = ({links}) => {
+const MenuOverlay = ({links}:{links: {title: string, path: string}[]}) => {
   return (
     <ul className='flex flex-col py-4 items-center'>
-        {links.map((link, index) => (
+        {links.map((link: { path: string; title: string }, index: React.Key | null | undefined) => (
         <li key={index}>
           <NavLink href={link.path} title={link.title} />
         </li>
     ))}
    </ul>
-    
   )
 }
 
